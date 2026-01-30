ba2533be2e625e576de844493d4143650b324c70
Nishant Garg <nishant2024rde@gmail.com>
2026-01-30 09:30:30 +0530

    img stitcher

--- pan_cap.py
+++ pan_cap.py
@@ -3,6 +3,28 @@ import cv2
 import glob
 import imutils
 
+image_path = glob.glob("imgs/*.jpeg")
+images = []
+
+for image in image_path:
+    img = cv2.imread(image)
+    images.append(img)
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+
+imageStitcher = cv2.Stitcher_create()
+
+error, stitched_img = imageStitcher.stitch(images)
+
+if not error:
+
+    cv2.imwrite("stiched_img.png", stitched_img)
+    cv2.imshow("stitched op", stitched_img)
+    cv2.waitKey(0)
+
+
+
+
 
 
 
